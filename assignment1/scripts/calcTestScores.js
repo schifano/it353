d7a50b547408113ce227ad7ba72f85f9abf1b7e5
Rachel Schifano <rschifa1@gmail.com>
2014-09-09 16:21:23 -0500

    Create scripts and add styles

---
+++ assignment1/scripts/calcTestScores.js
@@ -0,0 +1,6 @@
+// Given N test scores, write a JavaScript program to compute the (a) lowest, (b) highest, (c) average scores, and (d) list the scores that are higher than the average. Prompt the user for the value of N and the individual scores. You are to use the method "prompt" to do this question, not HTML form.
+
+// Prompt user for test scores
+// Indicate if user is finished
+// Calculate lowest, highest, average
+// List scores higher than average

b58d96ee6ff291bebfc04cbe909f70352f7ab1dc
Rachel Schifano <rschifa1@gmail.com>
2014-09-12 18:42:07 -0500

    Complete zip code and select validation

--- assignment1/scripts/validateZip.js
+++ assignment1/scripts/validateZip.js
@@ -6,33 +6,36 @@ function validateZip() {
     var zipLength = zipString.length; 
 
     // check zip code length
-    if (zipLength < 5) {
-        alert("Zip code must be at least 5 digits. You entered " + zipString.length + ".");
-        // return false;
+    if (zipLength < 5 || zipLength > 10) {
+        alert("Zip code must be between 5 and 9 digits. You entered " + zipString.length + ".");
+        return false;
     } else if (zipLength == 10 && zipString.charAt(5) == "-") {
-        // check that each value is a digit
-        for (i = 0; i < 4; i++) {
+        // check that the first five values are digits
+        for (i = 0; i <= 4; i++) {
             aChar = zipString.charAt(i);
             if (isNaN(aChar)) {
                 alert("Character " + aChar + " is not a digit.");
                 return false;
             }
-            return true;
         }
+        // check that last four values are digits
+        for (i = 6; i < 10; i++) {
+            aChar = zipString.charAt(i);
+            if (isNaN(aChar)) {
+                alert("Character " + aChar + " is not a digit.");
+                return false;
+            }
+        }
+        return true;
     } else {
         // check that each value is a digit
         for (i = 0; i < zipLength; i++) {
             aChar = zipString.charAt(i);
             if (isNaN(aChar)) {
                 alert("Character " + aChar + " is not a digit.");
+                return false;
             }
         }
     }
-
     return true;
 }
-
-
-/*(b) [6 pts] For zip code, the format is either (i) ddddd or 
-(ii) ddddd-dddd where d represents a digit.
-*/
